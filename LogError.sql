dedd22d4de770bf70a039865efa28f99b269d986
François LASSERRE <choiz@me.com>
2015-10-13 22:40:22 +0200

    Update and rename error.sql to LogError.sql

--- LogError.sql
+++ LogError.sql
@@ -1,12 +1,14 @@
-CREATE TABLE IF NOT EXISTS `error` (
+CREATE TABLE IF NOT EXISTS `LogError` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `type` varchar(10) NOT NULL,
     `file` varchar(255) NOT NULL,
     `msg` varchar(255) NOT NULL,
     `line` int(11) NOT NULL,
+    `column` int(11) NOT NULL,
+    `trace` varchar(255) NOT NULL,
     `url` varchar(255) NOT NULL,
     `useragent` varchar(255) NOT NULL,
     `session` varchar(255) NOT NULL,
     `date` datetime NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;
